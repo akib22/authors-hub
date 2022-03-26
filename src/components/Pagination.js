a53e0d94740d278cfb72acdc14b28b29964083e6
akib22 <adnanakib75@gmail.com>
2022-03-26 22:06:31 +0600

    Update position of the pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,28 +1,24 @@
-import { Pagination as BPagination, Row, Col } from 'react-bootstrap';
+import { Pagination as BPagination } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 function Pagination({ totalPage, handler, currentPage }) {
   return (
-    <Row className="d-flex align-items-center justify-content-center">
-      <Col sm={3}>
-        <BPagination>
-          <BPagination.First onClick={() => handler(1)} />
-          <BPagination.Prev
-            disabled={currentPage === 1}
-            onClick={() => handler(currentPage - 1)}
-          >
-            Prev
-          </BPagination.Prev>
-          <BPagination.Next
-            disabled={currentPage === totalPage}
-            onClick={() => handler(currentPage + 1)}
-          >
-            Next
-          </BPagination.Next>
-          <BPagination.Last onClick={() => handler(totalPage)} />
-        </BPagination>
-      </Col>
-    </Row>
+    <BPagination className="d-flex align-items-center justify-content-center mb-3">
+      <BPagination.First onClick={() => handler(1)} />
+      <BPagination.Prev
+        disabled={currentPage === 1}
+        onClick={() => handler(currentPage - 1)}
+      >
+        Prev
+      </BPagination.Prev>
+      <BPagination.Next
+        disabled={currentPage === totalPage}
+        onClick={() => handler(currentPage + 1)}
+      >
+        Next
+      </BPagination.Next>
+      <BPagination.Last onClick={() => handler(totalPage)} />
+    </BPagination>
   );
 }
 
